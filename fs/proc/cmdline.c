18fb296a0ced0f5abc890dff0bfe8d42d1692b0b
Sultanxda <sultanxda@gmail.com>
2017-05-22 15:41:38 -0700

    proc: Remove additional SafetyNet flags from /proc/cmdline
    SafetyNet checks androidboot.veritymode in Nougat, so remove it.
    
    Additionally, remove androidboot.enable_dm_verity and androidboot.secboot
    in case SafetyNet will check them in the future.
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -40,7 +40,6 @@ static void remove_safetynet_flags(char *cmd)
 static int __init proc_cmdline_init(void)
 {
 	strcpy(new_command_line, saved_command_line);
-
 	/*
 	 * Remove various flags from command line seen by userspace in order to
 	 * pass SafetyNet CTS check.
